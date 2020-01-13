1d4d878378296df2b9d60a2c1b238514cf6c0410
SnowBeLock <karkardro@gmail.com>
2020-01-13 20:43:52 +0200

    W

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,15 +12,23 @@ public class Controller extends Turn {
     public Spinner<Integer> userNum4;
     public TableView<Turn> turnList;
 
+    private int turnCounter;
+
     public void doTurn(ActionEvent actionEvent) {
     int n1=userNum1.getValue();
     int n2=userNum2.getValue();
     int n3=userNum3.getValue();
     int n4=userNum4.getValue();
+
+    turnCounter++;
+
+
     var guess=String.format("%d %d %d %d",n1,n2,n3,n4);
     var turn=new Turn();
+    turn.setTurnNr(turnCounter);
     turn.setUserGuess(guess);
-    turnList.getItems().add(turn);
+    turnList.getItems().add(0,turn);
+    turnList.sort();
 
     }
 }
